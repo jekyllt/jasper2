f2fa8ab77937dc56c79e780892a21205d3deb721
Fábio Madeira <fbiomadeira@gmail.com>
2017-11-21 16:43:23 +0000

    Improved atom feeds and now using jekyll-feed

--- _plugins/jekyll-tagsgenerator.rb
+++ _plugins/jekyll-tagsgenerator.rb
@@ -57,7 +57,7 @@ module Jekyll
       @site = site
       @base = base
       @dir = dir
-      @name = 'rss.xml'
+      @name = 'feed.xml'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), "tag.xml")
