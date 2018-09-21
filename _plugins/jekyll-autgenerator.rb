9ca0fb46252ef7d83c1c9dcd5c9e47e02f6ad3f2
Fábio Madeira <fbiomadeira@gmail.com>
2018-09-21 15:48:23 +0100

    Added feed.xml generator for authors and tags so that these also get atom feed.xml.

--- _plugins/jekyll-autgenerator.rb
+++ _plugins/jekyll-autgenerator.rb
@@ -13,7 +13,14 @@ module Jekyll
 
     def build_subpages(site, type, posts)
       posts[1] = posts[1].sort_by { |p| -p.date.to_f }
+      atomize(site, type, posts)
       paginate(site, type, posts)
+    end
+
+    def atomize(site, type, posts)
+      path = "/author/#{posts[0]}"
+      atom = AtomPageAuthor.new(site, site.source, path, type, posts[0], posts[1])
+      site.pages << atom
     end
 
     def paginate(site, type, posts)
@@ -49,6 +56,21 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), "author.html")
       self.data["grouptype"] = type
       self.data[type] = val
+    end
+  end
+
+  class AtomPageAuthor < Page
+    def initialize(site, base, dir, type, val, posts)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'feed.xml'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), "feed.xml")
+      self.data[type] = val
+      self.data["grouptype"] = type
+      self.data["posts"] = posts[0..9]
     end
   end
 end
--- _plugins/jekyll-tagsgenerator.rb
+++ _plugins/jekyll-tagsgenerator.rb
@@ -12,7 +12,14 @@ module Jekyll
 
     def build_subpages(site, type, posts)
       posts[1] = posts[1].sort_by { |p| -p.date.to_f }
+      atomize(site, type, posts)
       paginate(site, type, posts)
+    end
+
+    def atomize(site, type, posts)
+      path = "/tag/#{posts[0]}".gsub(' ', '-').downcase
+      atom = AtomPageTags.new(site, site.source, path, type, posts[0], posts[1])
+      site.pages << atom
     end
 
     def paginate(site, type, posts)
@@ -42,6 +49,21 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), "tag.html")
       self.data["grouptype"] = type
       self.data[type] = val
+    end
+  end
+
+  class AtomPageTags < Page
+    def initialize(site, base, dir, type, val, posts)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'feed.xml'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), "feed.xml")
+      self.data[type] = val
+      self.data["grouptype"] = type
+      self.data["posts"] = posts[0..9]
     end
   end
 end
