d175ce7a0a95229f8a815f55f068bd19e118a060
Jetroid <holtjet@gmail.com>
2020-05-09 13:34:22 +0100

    Handle tags with non standard characters

--- _plugins/jekyll-tagsgenerator.rb
+++ _plugins/jekyll-tagsgenerator.rb
@@ -1,3 +1,4 @@
+require 'slugify'
 module Jekyll
 
   class TagsGenerator < Generator
@@ -17,7 +18,7 @@ module Jekyll
     end
 
     def atomize(site, type, posts)
-      path = "/tag/#{posts[0]}".gsub(' ', '-').downcase
+      path = "/tag/" + posts[0].slugify
       atom = AtomPageTags.new(site, site.source, path, type, posts[0], posts[1])
       site.pages << atom
     end
@@ -26,7 +27,7 @@ module Jekyll
       pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
       (1..pages).each do |num_page|
         pager = Jekyll::Paginate::Pager.new(site, num_page, posts[1], pages)
-        path = "/tag/#{posts[0]}".gsub(' ', '-').downcase
+        path = "/tag/" + posts[0].slugify
         if num_page > 1
           path = path + "/page#{num_page}"
         end
